9ea0d249374c8d790f60829b90fcc7e33fb08fe9
Bohdan Tarovyk <westxnitro@gmail.com>
2026-02-07 16:56:42 +0100

    add 1 task

---
+++ src/js/1-timer.js
@@ -0,0 +1,88 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const startBtn = document.querySelector('button[data-start]');
+const input = document.querySelector('#datetime-picker');
+const daysEl = document.querySelector('[data-days]');
+const hoursEl = document.querySelector('[data-hours]');
+const minutesEl = document.querySelector('[data-minutes]');
+const secondsEl = document.querySelector('[data-seconds]');
+
+let userSelectedDate = null;
+let timerId = null;
+
+// Кнопка вимкнена, поки не обрана правильна дата
+startBtn.disabled = true;
+startBtn.classList.toggle('disable');
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const selectedDate = selectedDates[0];
+
+    if (selectedDate < new Date()) {
+      iziToast.error({
+        message: 'Please choose a date in the future',
+        position: 'topRight',
+      });
+      startBtn.disabled = true;
+    } else {
+      userSelectedDate = selectedDate;
+      startBtn.disabled = false;
+      startBtn.classList.toggle('disable'); // Активуємо кнопку, якщо дата вірна
+    }
+  },
+};
+
+flatpickr(input, options);
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+startBtn.addEventListener('click', () => {
+  // Блокуємо інпути після старту
+  startBtn.disabled = true;
+  input.disabled = true;
+  startBtn.classList.toggle('disable');
+
+  timerId = setInterval(() => {
+    const deltaTime = userSelectedDate - Date.now();
+
+    if (deltaTime <= 0) {
+      clearInterval(timerId);
+      input.disabled = false;
+      return;
+    }
+
+    const time = convertMs(deltaTime);
+    updateTimerInterface(time);
+  }, 1000);
+});
+
+// Окрема функція для оновлення інтерфейсу
+function updateTimerInterface({ days, hours, minutes, seconds }) {
+  daysEl.textContent = addLeadingZero(days);
+  hoursEl.textContent = addLeadingZero(hours);
+  minutesEl.textContent = addLeadingZero(minutes);
+  secondsEl.textContent = addLeadingZero(seconds);
+}
